3fbae552bbb44deb850e2e0108ca7974a183456b
YJOSH1 <yjoshi530@gmail.com>
2023-03-20 22:42:24 +1100

    Rewrote main function to handle new arguments

--- src/clubspark_extractor.py
+++ src/clubspark_extractor.py
@@ -3,125 +3,9 @@ Script to extract different types of membership lists from ClubSpark and if nece
 formatting on them before uploading to cloud file storage.
 """
 
-import os
-import sys
-import time
-import shutil
-import glob
 import argparse
-import pandas as pd
-from box_api import *
-from google_drive_api import *
-from dotenv import load_dotenv, find_dotenv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-
-load_dotenv(find_dotenv())
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service('/usr/local/bin/chromedriver'),
-                          options=chrome_options)
-
-
-def extract_list_from_clubspark(file_type, columns_to_delete_list=None):
-    delete_preexisting_lists(file_type)
-    print(get_divided_string('Extract List From ClubSpark'))
-    sign_in_to_clubspark()
-    download_list(file_type)
-    sign_out_of_clubspark()
-    if columns_to_delete_list is not None:
-        delete_columns_in_csv(columns_to_delete_list)
-    upload_list_to_box(file_type)
-    print('\nClubSpark members list extract complete.')
-
-
-def delete_preexisting_lists(file_type):
-    print(get_divided_string('Delete Pre-existing Lists'))
-    preexisting_lists_in_downloads = glob.glob(os.getenv('MEMBERSHIP_LIST_DOWNLOADS_PATH')
-                                               + '.' + file_type)
-    preexisting_lists_in_box = glob.glob(os.getenv('MEMBERSHIP_LIST_BOX_PATH') + '.' + file_type)
-
-    if len(preexisting_lists_in_downloads) > 0:
-        for membership_list in preexisting_lists_in_downloads:
-            os.remove(membership_list)
-        print(f'Deleted {len(preexisting_lists_in_downloads)} list(s) in the Downloads folder.')
-    else:
-        print('There are no lists in the Downloads folder to delete.')
-
-    if len(preexisting_lists_in_box) > 0:
-        for membership_list in preexisting_lists_in_box:
-            os.remove(membership_list)
-        print(f'Deleted {len(preexisting_lists_in_box)} list(s) in Box.')
-    else:
-        print('There are no lists in Box to delete.')
-
-
-def sign_in_to_clubspark():
-    # Get login page
-    driver.get('https://play.tennis.com.au/wheelershilltennisclub/Admin/Membership/members')
-    # Enter service account email
-    element = driver.find_element(By.ID, 'EmailAddress')
-    element.send_keys(os.getenv('CS_SERVICE_ACCOUNT_EMAIL'))
-    # Enter service account password
-    element = driver.find_element(By.ID, 'Password')
-    element.send_keys(os.getenv('CS_SERVICE_ACCOUNT_PASSWORD'))
-    # Click sign in button
-    driver.find_element(By.ID, 'signin-btn').click()
-    time.sleep(3)
-    print('Signed in to ClubSpark.')
-
-
-def download_list(file_type):
-    driver.find_element(By.CSS_SELECTOR, 'th.select-record > label:nth-child(1)').click()
-    driver.find_element(By.CSS_SELECTOR, 'a.btn:nth-child(2)').click()
-    driver.find_element(By.CSS_SELECTOR, '.btn-' + file_type).click()
-    time.sleep(3)
-    print('Downloaded list.')
-
-
-def sign_out_of_clubspark():
-    driver.get('https://play.tennis.com.au/wheelershilltennisclub/Account/SignOut')
-    driver.quit()
-    print('Signed out of ClubSpark.')
-
-
-def delete_columns_in_csv(columns_to_delete_list):
-    print(get_divided_string('Apply Formatting to CSV'))
-    downloaded_list = glob.glob(os.getenv('MEMBERSHIP_LIST_DOWNLOADS_PATH') + '.csv')
-    data = pd.read_csv(downloaded_list[0])
-    for column in columns_to_delete_list:
-        data.drop(column, inplace=True, axis=1)
-        print(f'Deleted column \'{column}\'')
-    data.to_csv(downloaded_list[0], index=False)
-
-
-def upload_list_to_box(file_type):
-    print(get_divided_string('Upload List to Box'))
-    downloaded_lists = glob.glob(os.getenv('MEMBERSHIP_LIST_DOWNLOADS_PATH') + file_type)
-    if len(downloaded_lists) > 0:
-        for membership_list in downloaded_lists:
-            shutil.move(membership_list, os.getenv('MEMBERSHIP_LIST_BOX_UPLOAD_PATH'))
-        print('List uploaded to Box.')
-    else:
-        quit_with_error('There is no list to upload to Box.')
-
-
-def quit_with_error(error_msg):
-    print(f'Error: {error_msg}', file=sys.stderr)
-    sys.exit(1)
-
-
-def get_bordered_string(string, char='#'):
-    border = char * (len(string) + 4)
-    return f'{border}\n{char} {string} {char}\n{border}'
-
-
-def get_divided_string(string, char='-'):
-    divider = char * len(string)
-    return f'\n{divider}\n{string}\n{divider}'
+from helper import quit_with_error, get_bordered_string, setup_driver, get_divided_string, \
+    extract_list_from_clubspark
 
 
 if __name__ == "__main__":
@@ -132,9 +16,9 @@ if __name__ == "__main__":
     parser.add_argument('-l', '--list-type', required=True, default='all members',
                         help='The type of list to be extracted from ClubSpark. '
                              'Accepted list types: \'All members\',\'Unpaid members\'')
-    parser.add_argument('-f', '--file-type', required=True,
+    parser.add_argument('-f', '--file-type', required=True, default='csv',
                         help='The file type of the list to be extracted from ClubSpark. '
-                             'Accepted file types: \'PDF\', \'CSV\'')
+                             'Accepted file types: \'CSV\', \'PDF\'')
     parser.add_argument('-d', '--delete-columns', default=None,
                         help='Comma separated list of column names in extracted CSV list to be '
                              'deleted.')
@@ -142,22 +26,50 @@ if __name__ == "__main__":
                         help='The location of where the extracted list will be uploaded to. '
                         'Accepted upload locations: \'Box\', \'Google Drive\'')
     args = parser.parse_args()
-    delimiter = ','
+
+    list_type_arg = args.list_type.lower()
+    file_type_arg = args.file_type.lower()
+    upload_location_arg = args.upload_location.lower()
+
+    if list_type_arg != 'all members' and list_type_arg != 'unpaid members':
+        quit_with_error('Please specify a valid list type. '
+                        'Accepted list types: \'All members\', \'Unpaid members\'')
+
+    if file_type_arg != 'csv' and file_type_arg != 'pdf':
+        quit_with_error('Please specify a valid file type. Accepted file types: \'CSV\', \'PDF\'')
+
+    if upload_location_arg != 'box' and upload_location_arg != 'google drive':
+        quit_with_error('Please specify a valid upload location. '
+                        'Accepted upload locations: \'Box\', \'Google Drive\'')
 
     print(get_bordered_string('CLUBSPARK EXTRACTOR'))
+    driver = setup_driver()
 
-    if args.file_type == 'pdf':
+    if args.file_type.lower() == 'csv':
         print(get_divided_string('Extract Settings'))
-        print('File type: PDF')
-        extract_list_from_clubspark('pdf')
-    elif args.file_type == 'csv':
-        print(get_divided_string('Extract Settings'))
-        print('File type: CSV')
+        print(f'List type: {list_type_arg}')
+        print(f'File type: {file_type_arg}')
         if args.delete_columns is not None:
-            columns_to_delete = args.delete_columns.split(delimiter)
+            columns_to_delete = args.delete_columns.split(',')
             print(f'Columns to delete: {columns_to_delete}')
-            extract_list_from_clubspark('csv', columns_to_delete)
+            print(f'Upload location: {upload_location_arg}')
+            try:
+                extract_list_from_clubspark('csv', columns_to_delete)
+            except Exception as e:
+                quit_with_error(f'ClubSpark Extractor failed to execute.\n{e}')
         else:
-            extract_list_from_clubspark('csv')
+            print(f'Upload location: {upload_location_arg}')
+            try:
+                extract_list_from_clubspark('csv')
+            except Exception as e:
+                quit_with_error(f'ClubSpark Extractor failed to execute.\n{e}')
     else:
-        quit_with_error('Please specify a valid file type. Accepted file types: pdf, csv.')
+        print(get_divided_string('Extract Settings'))
+        print(f'List type: {list_type_arg}')
+        print(f'File type: {file_type_arg}')
+        print(f'Upload location: {upload_location_arg}')
+        try:
+            extract_list_from_clubspark(driver, args.list_type.lower(), args.file_type.lower(),
+                                        args.upload_location.lower())
+        except Exception as e:
+            quit_with_error(f'ClubSpark Extractor failed to execute.\n{e}')
