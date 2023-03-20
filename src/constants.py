675d46736a87a3b83a219c0271c697664b669887
YJOSH1 <yjoshi530@gmail.com>
2023-03-20 21:01:27 +1100

    Added script constants

---
+++ src/constants.py
@@ -0,0 +1,21 @@
+import os
+
+import keyring
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+# ClubSpark Constants
+CLUBSPARK_SERVICE_ACCOUNT_EMAIL = 'webmaster@wheelershilltennisclub.com'
+CLUBSPARK_SERVICE_ACCOUNT_PASSWORD = keyring.get_password('ClubSpark Extractor - ClubSpark SA',
+                                                          CLUBSPARK_SERVICE_ACCOUNT_EMAIL)
+CLUBPSPARK_SIGN_IN_URL = 'https://play.tennis.com.au/wheelershilltennisclub/Admin/Membership/members'
+CLUBSPARK_SIGN_OUT_URL = 'https://play.tennis.com.au/wheelershilltennisclub/Account/SignOut'
+
+# Box Constants
+BOX_CLIENT_ID = keyring.get_password('ClubSpark Extractor - Box Client ID', 'Box Client ID')
+BOX_APP_TOKEN_PRIMARY = keyring.get_password('ClubSpark Extractor - Box App Token Primary',
+                                             'Box App Token Primary')
+
+# File System Path Constants
+LIST_DOWNLOAD_PATH = os.environ.get('LIST_DOWNLOAD_PATH')
