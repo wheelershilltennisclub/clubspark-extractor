0c0949c76967741c68ea00b1156344ddb81c7ff2
YJOSH1 <yjoshi530@gmail.com>
2023-03-20 23:27:46 +1100

    Added Box folder id's

--- src/constants.py
+++ src/constants.py
@@ -16,6 +16,11 @@ CLUBSPARK_SIGN_OUT_URL = 'https://play.tennis.com.au/wheelershilltennisclub/Acco
 BOX_CLIENT_ID = keyring.get_password('ClubSpark Extractor - Box Client ID', 'Box Client ID')
 BOX_APP_TOKEN_PRIMARY = keyring.get_password('ClubSpark Extractor - Box App Token Primary',
                                              'Box App Token Primary')
+BOX_ALL_MEMBERS_FOLDER_ID = '199099109731'
+BOX_UNPAID_MEMBERS_FOLDER_ID = '199098777969'
+
+# Google Drive Constants
 
 # File System Path Constants
 LIST_DOWNLOAD_PATH = os.environ.get('LIST_DOWNLOAD_PATH')
+DOWNLOADS_FOLDER_PATH = os.environ.get('DOWNLOADS_FOLDER_PATH')
